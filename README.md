f194df7112fed60b527ea5bf3213bda7370f1f81
JarryYang <40589259+JarryYang@users.noreply.github.com>
2019-08-12 20:17:03 +1200

    Some updates

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Learning-Notes
 #### Notes in terms of learning the *Python for Data Analysis: Data Wrangling with Pandas, Numpy, and IPython, Wes Mckinney*
+#### And some practices code
--- SomePythonPractices/Practice.py
+++ SomePythonPractices/Practice.py
@@ -17,3 +17,73 @@ class Solution:
             else:
                 a.append(i)
         return ''.join(a)
+    
+    #2. Output the occurance of each character in a string
+    def countOccurance(str):
+        count={}
+        for i in str:
+            count[i]=str.count(i)
+        return count
+    
+    #3.Find a specific number in an array
+    def targetFind(target, array):
+        n=len(array)
+        if n==0:
+            return 0
+        else:
+            for i in range(n):
+                if target in array[i]:
+                    print("Found the target")
+                    return target
+                else:
+                    print("No such number")
+                    
+    #Or 
+    def targetFindNew(target,array):
+        if len(array)==0:
+            print("The array cannot be empty")
+            return 0
+        else:
+            rown=len(array)
+            coln=len(array[0])
+            startrow=0
+            startcol=coln-1
+            while startrow<=rown-1 and startcol>=0:
+                if array[rown][startcol]==target:
+                    print("Found the number")
+                    return True,target
+                elif array[rown][startcol]<target:
+                    startrow += 1
+                    print("Move to next row: "+startrow)
+                elif array[rown][startcol]>target:
+                    rown -= 1
+                else:
+                    print("No such number")
+                    return False
+                
+            
+
+"""
+        int rows=array.length;
+        int cols=array[0].length;
+        int row = 0;
+        int col = cols-1;
+        while(row<=rows-1 && col>=0){
+                if(array[row][col]==target)
+                {
+                    return true;                    
+                }else if(array[row][col]<target){
+                    row = row + 1;
+                }else{
+                    col = col - 1;
+                }
+        }                
+        return false;
+"""
+        
+    
+    
+    
+    
+                    
+                    
