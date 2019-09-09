8e001bf509185a31f35403385d6615a0f3c4fb00
JarryYang <40589259+JarryYang@users.noreply.github.com>
2019-09-09 15:19:01 +1200

    Update Practice.py

--- SomePythonPractices/Practice.py
+++ SomePythonPractices/Practice.py
@@ -277,10 +277,8 @@ class Solution:
         :type s: str
         :rtype: bool
         """
-
         # The stack to keep track of opening brackets.
         stack = []
-
         # Hash map for keeping track of mappings. This keeps the code very clean.
         # Also makes adding more types of parenthesis easier
         mapping = {")": "(", "}": "{", "]": "["}
@@ -306,7 +304,52 @@ class Solution:
         # In the end, if the stack is empty, then we have a valid expression.
         # The stack won't be empty for cases like ((()
         return not stack
-    
+    #Jave Solution
+class Solution {
+
+  // Hash table that takes care of the mappings.
+  private HashMap<Character, Character> mappings;
+
+  // Initialize hash map with mappings. This simply makes the code easier to read.
+  public Solution() {
+    this.mappings = new HashMap<Character, Character>();
+    this.mappings.put(')', '(');
+    this.mappings.put('}', '{');
+    this.mappings.put(']', '[');
+  }
+
+  public boolean isValid(String s) {
+
+    // Initialize a stack to be used in the algorithm.
+    Stack<Character> stack = new Stack<Character>();
+
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+
+      // If the current character is a closing bracket.
+      if (this.mappings.containsKey(c)) {
+
+        // Get the top element of the stack. If the stack is empty, set a dummy value of '#'
+        char topElement = stack.empty() ? '#' : stack.pop();
+
+        // If the mapping for this bracket doesn't match the stack's top element, return false.
+        if (topElement != this.mappings.get(c)) {
+          return false;
+        }
+      } else {
+        // If it was an opening bracket, push to the stack.
+        stack.push(c);
+      }
+    }
+
+    // If the stack still contains elements, then it is an invalid expression.
+    return stack.isEmpty();
+  }
+}
+
+
+
+##########################################################################################
     #LeetCode: Length of last word
     #my stupid answer
     def lengthOfLastWord(self, s: str) -> int:
