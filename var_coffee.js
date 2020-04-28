6c39818b74d9a578b6a5c6e9cda6e8f20d6f6719
Jerry Yang <junqiyang68@gmail.com>
2020-04-28 16:18:54 +1200

    safa

---
+++ var_coffee.js
@@ -0,0 +1,16 @@
+var youDrink="coffee";
+
+var reverse = function(s){
+  return s.split("").reverse().join("");
+}
+
+var barista = {
+  str1: "ers",
+  str2: reverse("arap"),
+  str3: "met".
+  request: function(preference){
+    return preference + ".secret word:" + this.str2 + this.str3 + this.str1;
+  }
+};
+
+barista.request(yourDrink);
